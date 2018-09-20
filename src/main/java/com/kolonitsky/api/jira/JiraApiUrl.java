a243cc2c9f0181e66ff664052a53dc5db84381db
Alexey Kolonitsky <alexey.s.kolonitsky@gmail.com>
2018-09-20 15:42:51 +0300

    Calculate AI of local changes if they available

--- src/main/java/com/kolonitsky/api/jira/JiraApiUrl.java
+++ src/main/java/com/kolonitsky/api/jira/JiraApiUrl.java
@@ -4,5 +4,5 @@ package com.kolonitsky.api.jira;
  * @author Alexey Kolonitsky &lt;alexey.s.kolonitsky@gmail.com&gt;
  */
 public class JiraApiUrl {
-	public static final String ISSUE = "/rest/api/2/issue/{issueKey}";
+	public static final String ISSUE = "/rest/api/2/issue/${issueKey}";
 }
--- src/main/java/com/kolonitsky/api/stash/StashApiUrl.java
+++ src/main/java/com/kolonitsky/api/stash/StashApiUrl.java
@@ -5,13 +5,13 @@ package com.kolonitsky.api.stash;
  */
 public class StashApiUrl {
 	// pull-request
-	public static String PULL_REQUEST = "/rest/api/1.0/projects/{projectKey}/repos/{projectSlug}/pull-requests/{pullRequestId}";
-	public static String PULL_REQUEST_APPROVE = "/rest/api/1.0/projects/{projectKey}/repos/{projectSlug}/pull-requests/{pullRequestId}/approve";
-	public static String PULL_REQUESTS = "/rest/api/1.0/projects/{projectKey}/repos/{projectSlug}/pull-requests?state={state}&limit=1000";
-	public static String PULL_REQUEST_COMMITS = "/rest/api/1.0/projects/{projectKey}/repos/{projectSlug}/pull-requests/{pullRequestId}/commits";
-	public static String PULL_REQUEST_TASKS = "/rest/api/1.0/projects/{projectKey}/repos/{projectSlug}/pull-requests/{pullRequestId}/tasks";
+	public static String PULL_REQUEST = "/rest/api/1.0/projects/${projectKey}/repos/${projectSlug}/pull-requests/${pullRequestId}";
+	public static String PULL_REQUEST_APPROVE = "/rest/api/1.0/projects/{$projectKey}/repos/${projectSlug}/pull-requests/${pullRequestId}/approve";
+	public static String PULL_REQUESTS = "/rest/api/1.0/projects/${projectKey}/repos/${projectSlug}/pull-requests?state=${state}&limit=1000";
+	public static String PULL_REQUEST_COMMITS = "/rest/api/1.0/projects/${projectKey}/repos/${projectSlug}/pull-requests/${pullRequestId}/commits";
+	public static String PULL_REQUEST_TASKS = "/rest/api/1.0/projects/${projectKey}/repos/${projectSlug}/pull-requests/${pullRequestId}/tasks";
 	// Comments
-	public static String PULL_REQUEST_COMMENTS = "/rest/api/1.0/projects/{projectKey}/repos/{projectSlug}/pull-requests/{pullRequestId}/comments";
+	public static String PULL_REQUEST_COMMENTS = "/rest/api/1.0/projects/${projectKey}/repos/${projectSlug}/pull-requests/${pullRequestId}/comments";
 	// Tasks
 	public static String TASKS = "/rest/api/1.0/tasks";
 
